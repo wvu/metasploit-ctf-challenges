aabb790b4f120fa404bf427f01b09cf61458bae4
William Vu <William_Vu@rapid7.com>
2019-04-17 20:18:02 -0500

    Update build.sh for distribution

--- 2018/2_of_diamonds/build.sh
+++ 2018/2_of_diamonds/build.sh
@@ -1,7 +1,6 @@
 #!/bin/sh -ex
 
-# XXX: Cap CPU at 10% due to SIMH bug
-DOCKER_FLAGS="$DOCKER_FLAGS --cpus .1"
+docker build -t 2_of_diamonds .
 
-docker build -t 2_of_diamonds "${BUILD_DIR:-..}/2_of_diamonds"
-docker run -dp 25:25 -p 79:79 --name 2_of_diamonds $DOCKER_FLAGS 2_of_diamonds
+# XXX: Cap CPU at 10% due to SIMH bug
+docker run -dp 25:25 -p 79:79 --name 2_of_diamonds --cpus .1 2_of_diamonds
