352ae1c237cb8fe6fe32240cf250bfc1b3fd5b81
William Vu <William_Vu@rapid7.com>
2020-02-03 18:33:16 -0600

    Update build.sh for distribution

--- 2019/2_of_diamonds/build.sh
+++ 2019/2_of_diamonds/build.sh
@@ -1,14 +1,6 @@
 #!/bin/sh -ex
 
+docker build -t 2_of_diamonds .
+
 # XXX: Cap CPU at 30% due to high idle
-DOCKER_FLAGS="$DOCKER_FLAGS --rm --cpus 0.3"
-RUN_CMD="docker run -dp 25:25 -p 79:79 --name 2_of_diamonds $DOCKER_FLAGS 2_of_diamonds"
-
-docker build -t 2_of_diamonds "${BUILD_DIR:-..}/2_of_diamonds"
-
-if [ -n "$STARTUP_FILE" ]; then
-  echo "$RUN_CMD" >> "$STARTUP_FILE"
-  exit
-fi
-
-$RUN_CMD
+docker run -dp 25:25 -p 79:79 --rm --name 2_of_diamonds --cpus 0.3 2_of_diamonds
